aa6a1fed672bbcccbaa12870c2d68a2ce2c6e47c
Farah <farahcadet@gmail.com>
2020-12-09 18:29:12 -0500

    profile page components, formatting needed

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -4,14 +4,20 @@ const Navbar = () => {
   return (
     <div>
       <div className="NavBarContainer">
-        <div className="NavLinks"><a href='/' className="NavLink">HOME</a><a href='/aboutus' className="NavLink">ABOUT US</a><a href='/login' className="NavLink">ACCOUNT</a></div>
-        <div className="NavTitle">MIND-LINK</div>
-        <div> 
-        <input
-          type="text"
-          className="navsearch"
-          placeholder="      SEARCH"
-        /></div>
+      <div className="NavTitle">MIND-LINK</div>
+        <div className="NavLinks"><a href='/' className="NavLink">BROWSE</a><a href='/aboutus' className="NavLink">PLAYLISTS</a><a href='/login' className="NavLink">SEARCH</a></div>
+        <div className='navBarRight'> 
+        <div className="uploadlink"><a href='/' className="navupload">UPLOAD</a></div>
+        <div id='navuser'>
+    </div>
+    <div id='navName'>Name</div>
+    <a href='/' >
+      <div id='navbell'></div>
+    </a>
+    <a href='/' >
+      <div id='navdm'></div>
+    </a>
+        </div>
       </div>
     </div>
   )
--- client/src/Pages/ProfilePage.js
+++ client/src/Pages/ProfilePage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Profile from '../Components/Profile'
-import AudioBar from '../Components/AudioBar'
+// import AudioBar from '../Components/AudioBar'
 
 const ProfilePage = () => {
   return (
     <div>
       <Navbar/>
       <Profile/>
-      <AudioBar/>
+      {/* <AudioBar/> */}
     </div>
   )
 }
